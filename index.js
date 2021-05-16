f5a80137e7c3d86466e6bca17eea1d4265468f37
hfurkandemirci <76955977+hfurkandemirci@users.noreply.github.com>
2021-05-16 23:10:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+﻿const Discord = require('discord.js') // discord.js modülü tanımlıyoruz.
+const client = new Discord.Client() // client tanımalamsı
+const { readdirSync } = require('fs'); // tanımlamalar
+const { join } = require('path'); // tanımlamalar
+
+client.commands= new Discord.Collection(); // komutları alıyoruz
+
+const prefix = "*"
+
+const commandFiles = readdirSync(join(__dirname, "komutlar")).filter(file => file.endsWith(".js")); // Belli bir klasörden belli .js uzantılı dosyaları buluyor.
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "komutlar", `${file}`));
+    client.commands.set(command.kod, command); // Komutları Ayarlıyoruz.
+}
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+    client.user.setActivity('*yardım')
+    console.log('Bot Aktif')
+});
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return message.channel.send(`Komut dosyamda **${command}** adlı bir komut bulamadım.`);
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+client.login('ODQzMTUyNzE0OTc5OTk5Nzg1.YJ_tKA.2z1VOHZ0TB-YN-sncKAtLiK__LE')
